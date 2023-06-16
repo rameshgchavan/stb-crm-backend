f6ec306d8077ea9f2f06a3dcfe604d153377513a
Ramesh_Chavan <ramesh7452@gmail.com>
2023-06-16 23:57:35 +0530

    rearraged code

--- server.js
+++ server.js
@@ -8,12 +8,33 @@ const UsersRoutes = require("./src/routes/UsersRoutes");
 const CustomersRoutes = require("./src/routes/CustomersRoutes");
 const TransactionsRoutes = require("./src/routes/TransactionsRoutes");
 
+
+// Create object of express
+const app = express();
+
+// Use middlewares and routes in express
+// app.use(cors());
+app.use(express.json());
+app.use("/users", UsersRoutes);
+app.use("/customers", CustomersRoutes);
+app.use("/transactions", TransactionsRoutes);
+
+const options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+    index: ['index.html'],
+    maxAge: '1m',
+    redirect: false
+  }
+
+//Run frontend
+app.use(express.static('./client/build',options)); //Note: Copy build folder of frontend and paste it into backend
+
 // Environment setting
 dotEnv.config();
 const PORT = process.env.PORT;
 
-// Create object of express
-const app = express();
 
 app.listen(PORT || 8080, () => {
     try {
@@ -23,13 +44,3 @@ app.listen(PORT || 8080, () => {
         console.log("Connection failed for reason: " + err);
     }
 })
-
-// Use middlewares and routes in express
-// app.use(cors());
-app.use(express.json());
-app.use("/users", UsersRoutes);
-app.use("/customers", CustomersRoutes);
-app.use("/transactions", TransactionsRoutes);
-
-//Run frontend
-app.use(express.static('./client/build')); //Note: Copy build folder of frontend and paste it into backend
