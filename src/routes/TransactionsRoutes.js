605352265277f84d222493f8d8120b93b68a161d
Ramesh_Chavan <ramesh7452@gmail.com>
2023-11-16 01:26:23 +0530

    added api to count recharged STBs

--- src/routes/TransactionsRoutes.js
+++ src/routes/TransactionsRoutes.js
@@ -18,7 +18,7 @@ const TransactionsSchema = require("../models/TransactionsModel");
 const TransactionsRoutes = express.Router();
 
 // (APIs) downwards
-// HTTP request get method to get transactions
+// HTTP request post method to get transactions
 TransactionsRoutes.route("/").post(TokenVerification, async (req, res) => {
     const { dbName, collectionName, acNo } = req.body;
 
@@ -29,6 +29,41 @@ TransactionsRoutes.route("/").post(TokenVerification, async (req, res) => {
 
     acNo ? res.send(await TransactionsModel.find({ AcNo: acNo }))
         : res.send(await TransactionsModel.find());
+})
+
+// HTTP request post method to get recharged stb count 
+TransactionsRoutes.route("/rcstbcount").post(TokenVerification, async (req, res) => {
+    const monthsList = [
+        "Jan", "Feb", "Mar", "Apr",
+        "May", "Jun", "Jul", "Aug",
+        "Sep", "Oct", "Nov", "Dec"
+    ];
+
+    let rcSTBCount = [];
+
+    const { dbName, ofYear } = req.body;
+
+    const connection = mongoDBConnection.useDb(dbName, { useCache: true });
+
+    for (let i = 0; i < monthsList.length; i++) {
+        const TransactionsModel = connection.models[`${monthsList[i]}-${ofYear}`]
+            || connection.model(`${monthsList[i]}-${ofYear}`, TransactionsSchema);
+
+        rcSTBCount.push(await TransactionsModel.count({
+            $and: [
+                {
+                    $or: [
+                        { PlanType: "Basic" },
+                        { PlanType: "Hathway Bouquet" }
+                    ]
+                },
+                { TransactionType: { $ne: "Cancellation" } }
+            ]
+        })
+        )
+    }
+
+    res.send(rcSTBCount);
 })
 
 // Export Router
