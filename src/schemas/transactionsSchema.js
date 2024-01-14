b451d7922333f65ed776032b21938ba7fc7f4226
Ramesh_Chavan <ramesh7452@gmail.com>
2024-01-15 04:40:32 +0530

    comment updated

--- src/schemas/transactionsSchema.js
+++ src/schemas/transactionsSchema.js
@@ -1,7 +1,7 @@
 // Import mongoose
 const mongoose = require("mongoose");
 
-// Create Schema
+// This schema is created to handle collections: package-customers, packages-bills and statistics
 const transactionsSchema = new mongoose.Schema({
     yearMonth: String,
     data: Object
